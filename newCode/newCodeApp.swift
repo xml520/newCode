2e8e2c1eef791589c7e64011a6d2530b3f9326a2
xieminglin <1150383838@qq.com>
2022-11-06 01:56:32 +0800

    Initial Commit

---
+++ newCode/newCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  newCodeApp.swift
+//  newCode
+//
+//  Created by 谢明霖 on 2022/11/6.
+//
+
+import SwiftUI
+
+@main
+struct newCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
